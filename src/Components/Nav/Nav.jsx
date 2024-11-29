aa18c345dfb83399b602ca0bd785de4c5cbfdf37
Davi Pereira <dp.junior1997@gmail.com>
2024-11-29 19:23:43 -0300

    fix: fixing hero component size in mobile screen

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,55 +1,86 @@
-import React, { useState } from 'react';
-import { Drawer, IconButton, List, ListItem, Button, Box  } from '@mui/material';
-import frame from '../../Assets/Imagens/Frame.png';
-import frame2 from '../../Assets/Imagens/Frame2.png';
-import hamburguer from '../../Assets/Imagens/hamburguer.png';
-import './Nav.css';
-
+import React, { useState } from "react";
+import { Drawer, IconButton, List, ListItem, Button, Box } from "@mui/material";
+import frame from "../../Assets/Imagens/Frame.png";
+import frame2 from "../../Assets/Imagens/Frame2.png";
+import hamburguer from "../../Assets/Imagens/hamburguer.png";
+import "./Nav.css";
 
 export const Nav = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setIsDrawerOpen(open);
+  };
 
-    const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-          return;
-        }
-        setIsDrawerOpen(open);
-      };
-    
   return (
-    <div className='nav'>
-        <div className="logo">
-            <img className='logo' src={frame} alt="" />
-            <img className='logo-p' src={frame2} alt="" />
-        </div>
+    <div className="nav">
+      <div className="logo">
+        <img className="logo" src={frame} alt="" />
+        <img className="logo-p" src={frame2} alt="" />
+      </div>
 
-        <IconButton
-            edge="end"
-            aria-label="menu"
-            onClick={toggleDrawer(true)}
-            sx={{ display: { sm: 'block', md: 'none' }, 
-                color: '#8176AF', 
-                position: 'absolute', 
-                top: '89px', 
-                right: '50px',
-
-            }} 
-        >
+      <IconButton
+        edge="end"
+        aria-label="menu"
+        onClick={toggleDrawer(true)}
+        sx={{
+          display: { sm: "block", md: "none" },
+          color: "#8176AF",
+          position: "absolute",
+          top: "89px",
+          right: "50px",
+          zIndex: 1,
+        }}
+      >
         <img src={hamburguer} alt="" />
       </IconButton>
       <div className="menu">
-                <ul>
-                    <li>ABOUT</li>
-                    <li>SERVICES</li>
-                    <li>TECHNOLOGIES</li>
-                    <li>HOW TO</li>
-                </ul>
-                <div className="contato">
-                    <Button variant="outlined" className="btn1" sx= {{ width: 154, height: 48, borderRadius: '40px', fontFamily: 'Montserrat, sans-serif', fontSize: '12px', color: 'white', borderColor: "white" }}>CONTACT US</Button>
-                    <Button variant="contained" className="btn2"  sx={{ width: 154, height: 48, borderRadius: '40px', fontFamily: 'Montserrat, sans-serif', fontSize: '12px', fontWeight: 'bold', color: '#343045', background: 'linear-gradient(90deg, #8176AF, #C0B7E8)' }}>JOIN HYDRA</Button>
-                </div>
+        <ul>
+          <li>ABOUT</li>
+          <li>SERVICES</li>
+          <li>TECHNOLOGIES</li>
+          <li>HOW TO</li>
+        </ul>
+        <div className="contato">
+          <Button
+            variant="outlined"
+            className="btn1"
+            sx={{
+              width: 154,
+              height: 48,
+              borderRadius: "40px",
+              fontFamily: "Montserrat, sans-serif",
+              fontSize: "12px",
+              color: "white",
+              borderColor: "white",
+            }}
+          >
+            CONTACT US
+          </Button>
+          <Button
+            variant="contained"
+            className="btn2"
+            sx={{
+              width: 154,
+              height: 48,
+              borderRadius: "40px",
+              fontFamily: "Montserrat, sans-serif",
+              fontSize: "12px",
+              fontWeight: "bold",
+              color: "#343045",
+              background: "linear-gradient(90deg, #8176AF, #C0B7E8)",
+            }}
+          >
+            JOIN HYDRA
+          </Button>
         </div>
+      </div>
 
       <Drawer
         anchor="right"
@@ -58,67 +89,70 @@ export const Nav = () => {
         className="custom-drawer"
       >
         <Box
-         role="presentation"
-         onClick={toggleDrawer(false)}
-         onKeyDown={toggleDrawer(false)}
-        sx={{
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+          sx={{
             width: 260,
-            padding: '20px',
-            height: '80%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            gap: '5px'
-        }}
+            padding: "20px",
+            height: "80%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: "5px",
+          }}
         >
-         <List sx={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
-                <ListItem button>ABOUT</ListItem>
-                <ListItem button>SERVICES</ListItem>
-                <ListItem button>TECHNOLOGIES</ListItem>
-                <ListItem button>HOW TO</ListItem>
-         </List>
-          <Box 
-           sx={{ 
-                marginTop: '20px', 
-                display: 'flex', 
-                flexDirection: 'column', 
-                gap: '10px' 
-             }}>
-                <Button 
-                    variant="outlined" 
-                    className="btn1" 
-                    sx={{ 
-                        width: 154, 
-                        height: 48, 
-                        borderRadius: '40px', 
-                        fontFamily: 'Montserrat, sans-serif', 
-                        fontSize: '12px', 
-                        color: 'white', 
-                        borderColor: "white" 
-                    }}>
-                    CONTACT US
-                </Button>
-                <Button  
-                    variant="contained" 
-                    className="btn2" 
-                    sx={{ 
-                    width: 154,
-                     height: 48, 
-                     borderRadius: '40px', 
-                     fontFamily: 'Montserrat, sans-serif', 
-                     fontSize: '12px', 
-                     fontWeight: 'bold', 
-                     color: '#343045', 
-                     background: 'linear-gradient(90deg, #8176AF, #C0B7E8)' 
-                     }}>
-                    JOIN HYDRA
-                </Button>
-              </Box>
-            </Box>
-        </Drawer>
+          <List sx={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+            <ListItem button>ABOUT</ListItem>
+            <ListItem button>SERVICES</ListItem>
+            <ListItem button>TECHNOLOGIES</ListItem>
+            <ListItem button>HOW TO</ListItem>
+          </List>
+          <Box
+            sx={{
+              marginTop: "20px",
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+            }}
+          >
+            <Button
+              variant="outlined"
+              className="btn1"
+              sx={{
+                width: 154,
+                height: 48,
+                borderRadius: "40px",
+                fontFamily: "Montserrat, sans-serif",
+                fontSize: "12px",
+                color: "white",
+                borderColor: "white",
+              }}
+            >
+              CONTACT US
+            </Button>
+            <Button
+              variant="contained"
+              className="btn2"
+              sx={{
+                width: 154,
+                height: 48,
+                borderRadius: "40px",
+                fontFamily: "Montserrat, sans-serif",
+                fontSize: "12px",
+                fontWeight: "bold",
+                color: "#343045",
+                background: "linear-gradient(90deg, #8176AF, #C0B7E8)",
+              }}
+            >
+              JOIN HYDRA
+            </Button>
+          </Box>
+        </Box>
+      </Drawer>
     </div>
-  )
-}
+  );
+};
 
 export default Nav;
